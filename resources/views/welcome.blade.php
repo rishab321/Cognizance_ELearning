eaa7c27c3f37ea7fef7f112e41c6532a9a5d8d28
Rishab kumar <rishabh9547@gmail.com>
2024-03-05 15:16:18 +0530

    implement frontend content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,12 +18,13 @@
                             @endphp
                             @foreach ($categories as $category)
                                 <div class="col-lg-4 col-md-6 col-12">
-                                  <div class="card" style="width: 15rem;">
-                                    <img src="{{asset('uploads/category/'.$category->image)}}" class="card-img-top" alt="...">
+                                  <div class="card" style="width: 18rem;">
+                                    <img src="{{asset('uploads/category/'.$category->image)}}" 
+                                    class="card-img-top" alt="...">
                                     <a href="#" class="btn-buy"></a>
                                     <div class="card-body">
                                       <h5 class="card-title">{{ $category->title }}</h5>
-                                      <p class="card-text">Some quick example text to build on the card </p>
+                                      <p class="card-text"></p>
                                       <a href="#" class="btn btn-primary">Buy Now</a>
                                     </div>
                                   </div>
